a04e563b9e8c080c7ecb70619058d7eb68d73df5
SivaPrakashNiagara <striker.niagara@gmail.com>
2025-02-19 12:44:21 +0530

    worked in payloadConversion function

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:oro_drip_irrigation/Constants/mqtt_manager_mobile.dart'
 if (dart.library.html) 'package:oro_drip_irrigation/Constants/mqtt_manager_web.dart';
 import 'package:flutter/material.dart';
 import 'package:oro_drip_irrigation/StateManagement/config_maker_provider.dart';
+import 'package:oro_drip_irrigation/utils/environment.dart';
 import 'package:oro_drip_irrigation/utils/network_utils.dart';
 import 'package:provider/provider.dart';
 import 'Constants/env_setup.dart';
@@ -11,7 +12,7 @@ import 'app/app.dart';
 
 void main() {
   NetworkUtils.initialize();
-  GlobalConfig.setEnvironment(Environment.development);
+  // GlobalConfig.setEnvironment(Environment.development);
   MqttManager mqttManager = MqttManager();
   mqttManager.initializeMQTTClient();
   mqttManager.connect();
