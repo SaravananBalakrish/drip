30a922e824db08133889a60768bd48a8ba77b45b
Niagara2024 <niagarakamaraj@gmail.com>
2025-05-05 10:28:54 +0530

    code updated

--- lib/Screens/planning/WeatherScreenExternal.dart
+++ lib/Screens/planning/WeatherScreenExternal.dart
@@ -1,7 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'dart:html' as html;
-import 'dart:ui_web' as ui;
 
 class WebViewExample extends StatelessWidget {
   final int userid; // Define the userid field
@@ -38,21 +36,21 @@ class _IframeState extends State<Iframe> {
     iframeViewType = 'iframe-${DateTime.now().millisecondsSinceEpoch}';
 
     // Register the iframe view with the platform view registry
-    ui.platformViewRegistry.registerViewFactory(iframeViewType, (int viewId) {
+    /*ui.platformViewRegistry.registerViewFactory(iframeViewType, (int viewId) {
       var iframe = html.IFrameElement();
       iframe.src = widget.urlstr;
       iframe.style.border = 'none'; // Optional: removes border
       return iframe;
-    });
+    });*/
   }
 
   @override
   void dispose() {
     // Hide the iframe (not unregistering the view)
-    final iframe = html.document.querySelector('iframe');
+    /*final iframe = html.document.querySelector('iframe');
     if (iframe != null) {
       iframe.style.display = 'none'; // Hide the iframe if it's present
-    }
+    }*/
 
     super.dispose();
   }
