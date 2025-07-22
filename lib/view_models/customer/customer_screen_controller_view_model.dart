958ba502d54ee5aa3cdc1fbe583006122921afae
unknown <saravananbmsd@gmail.com>
2025-07-22 14:52:14 +0530

    gem model list modified

--- lib/view_models/customer/customer_screen_controller_view_model.dart
+++ lib/view_models/customer/customer_screen_controller_view_model.dart
@@ -319,7 +319,7 @@ class CustomerScreenControllerViewModel extends ChangeNotifier {
       return;
     }
 
-    final isCategory1 = AppConstants.gemModelList.contains(mySiteList.data[sIndex].master[mIndex].modelId);
+    final isCategory1 = [...AppConstants.gemModelList, ...AppConstants.ecoGemModelList].contains(mySiteList.data[sIndex].master[mIndex].modelId);
     final payload = isCategory1
         ? jsonEncode({"3000": {"3001": ""}})
         : jsonEncode({"sentSms": "#live"});
