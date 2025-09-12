7743b0494de41d17992f71e01a0f808525445c84
N.S.Venkatesasn <venkatesan.niagara@gmail.com>
2025-09-12 12:59:27 +0530

    pump topic

--- lib/view_models/login_view_model.dart
+++ lib/view_models/login_view_model.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../repository/repository.dart';
 import '../utils/shared_preferences_helper.dart';
 
@@ -28,7 +26,7 @@ class LoginViewModel extends ChangeNotifier {
   void initState() {
     mobileNoController = TextEditingController();
     passwordController = TextEditingController();
-   }
+  }
 
   void onIsObscureChanged() {
     isObscure = !isObscure;
@@ -49,18 +47,9 @@ class LoginViewModel extends ChangeNotifier {
     }
   }
   Future<void> login() async {
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-    await messaging.getToken().then((String? token) async{
-       final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('deviceToken', token ?? '' );
-     });
-    final token = await PreferenceHelper.getDeviceToken();
-
     isLoading = true;
     errorMessage = "";
     notifyListeners();
-
-
 
     try {
       String mobileNumber = mobileNoController.text.trim();
@@ -76,23 +65,20 @@ class LoginViewModel extends ChangeNotifier {
         errorMessage = "Invalid Mobile number or Password!";
         notifyListeners();
         return;
-      } else if(token == null || token.isEmpty){
-        print("token in the else :: $token");
-        isLoading = false;
-        errorMessage = "Device token not generated";
-        notifyListeners();
-        return;
       }
 
-       String cleanedCountryCode = countryCode.replaceAll("+", "");
+      final token = await PreferenceHelper.getDeviceToken();
+
+      String cleanedCountryCode = countryCode.replaceAll("+", "");
       Map<String, Object> body = {
         'countryCode' : cleanedCountryCode,
         'mobileNumber': mobileNumber,
         'password': password,
-        'deviceToken': token ?? '',
+        'deviceToken': token ?? 'null',
         'isMobile' : kIsWeb? false : true,
       };
-       final response = await repository.checkLoginAuth(body);
+
+      final response = await repository.checkLoginAuth(body);
       final data = jsonDecode(response.body);
       if (response.statusCode == 200 && data['code'] == 200) {
         final userData = data['data']['user'];
@@ -104,7 +90,7 @@ class LoginViewModel extends ChangeNotifier {
           countryCode: cleanedCountryCode,
           mobileNumber: mobileNumber,
           email: userData['email'],
-         );
+        );
         onLoginSuccess(data['message']);
       } else {
         isLoading = false;
