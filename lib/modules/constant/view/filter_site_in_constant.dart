705e40061dbf12af10deb081c83d19b57eed685e
SivaPrakashNiagara <striker.niagara@gmail.com>
2025-09-13 13:13:43 +0530

    taken from stash list

--- lib/modules/constant/view/filter_site_in_constant.dart
+++ lib/modules/constant/view/filter_site_in_constant.dart
@@ -10,7 +10,10 @@ import 'package:oro_drip_irrigation/Screens/Constant/main_valve_in_constant.dart
 import 'package:oro_drip_irrigation/Screens/Constant/main_valve_in_constant.dart';
 import 'package:oro_drip_irrigation/modules/constant/model/object_in_constant_model.dart';
 
+import '../../../Constants/dialog_boxes.dart';
 import '../../../StateManagement/overall_use.dart';
+import '../../../repository/repository.dart';
+import '../../../services/http_service.dart';
 import '../../../services/mqtt_service.dart';
 import '../../../utils/constants.dart';
 import '../../../utils/environment.dart';
@@ -29,111 +32,5 @@ class FilterSiteInConstant extends StatefulWidget {
 class _FilterSiteInConstantState extends State<FilterSiteInConstant> {
   double cellWidth = 200;
   MqttService mqttService = MqttService();
+  final Repository repository = Repository(HttpService());
 
-
-  @override
-  Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    int settingLength = widget.constPvd.defaultFilterSiteSetting.where((setting) {
-      if(AppConstants.gemModelList.contains(widget.constPvd.userData['modelId'])){
-        return setting.gemDisplay;
-      }else{
-        return setting.ecoGemDisplay;
-      }
-    }).length;
-    settingLength = settingLength + 1;
-    double minWidth = (cellWidth * 1) + (settingLength * cellWidth) + 50;
-    Color borderColor = const Color(0xffE1E2E3);
-    return DataTable2(
-      border: TableBorder(
-        top: BorderSide(color: borderColor, width: 1),
-        bottom: BorderSide(color: borderColor, width: 1),
-        left: BorderSide(color: borderColor, width: 1),
-        right: BorderSide(color: borderColor, width: 1),
-      ),
-      minWidth: minWidth,
-      fixedLeftColumns: minWidth < screenWidth ? 0 : 1,
-      columns: [
-        DataColumn2(
-            headingRowAlignment: MainAxisAlignment.center,
-            fixedWidth: cellWidth,
-            label: Text('Filter Site', style: Theme.of(context).textTheme.labelLarge,textAlign: TextAlign.center, softWrap: true)
-        ),
-        ...widget.constPvd.defaultFilterSiteSetting
-            .where((defaultSetting) => AppConstants.gemModelList.contains(widget.constPvd.userData['modelId']) ? defaultSetting.gemDisplay : defaultSetting.ecoGemDisplay)
-            .map((defaultSetting) {
-          return DataColumn2(
-              headingRowAlignment: MainAxisAlignment.center,
-              fixedWidth: cellWidth,
-              label: Text(defaultSetting.title, style: Theme.of(context).textTheme.labelLarge,textAlign: TextAlign.center, softWrap: true,)
-          );
-        }),
-        DataColumn2(
-            headingRowAlignment: MainAxisAlignment.center,
-            fixedWidth: cellWidth,
-            label: Text('Backwash Command ono/ff', style: Theme.of(context).textTheme.labelLarge,textAlign: TextAlign.center, softWrap: true)
-        ),
-      ],
-      rows: List.generate(widget.constPvd.filterSite.length, (row){
-        ObjectInConstantModel filterSite = widget.constPvd.filterSite[row];
-        return DataRow2(
-            color: WidgetStatePropertyAll(
-              row.isOdd ? Colors.white : const Color(0xffF8F8F8),
-            ),
-            cells: [
-              DataCell(
-                  Center(child: Text(filterSite.name.toString(), textAlign: TextAlign.center, style: TextStyle(color: Theme.of(context).primaryColorLight),))
-              ),
-              ...filterSite.setting
-                  .where((defaultSetting) => AppConstants.gemModelList.contains(widget.constPvd.userData['modelId']) ? defaultSetting.gemDisplay : defaultSetting.ecoGemDisplay)
-                  .map((setting) {
-                return DataCell(
-                    AnimatedBuilder(
-                      animation: setting.value,
-                      builder: (context, child){
-                        return FindSuitableWidget(
-                          constantSettingModel: setting,
-                          onUpdate: (value){
-                            setting.value.value = value;
-                          },
-                          onOk: (){
-                            setting.value.value = widget.overAllPvd.getTime();
-                            Navigator.pop(context);
-                          },
-                          popUpItemModelList: widget.constPvd.filterSiteWhileBackwash,
-                        );
-                      },
-                    )
-
-                );
-              }),
-              DataCell(
-                  Center(
-                      child:  FilledButton.icon(
-                        icon: const Icon(Icons.swipe),
-                        onPressed: () {
-                          String manualBackwashPayload = jsonEncode({
-                            "4000": {"4001": filterSite.sNo.toString()}
-                          });
-                          mqttService.topicToPublishAndItsMessage(manualBackwashPayload, '${Environment.mqttPublishTopic}/${widget.constPvd.userData['deviceId']}');
-
-                        },
-                        label: const Text('Manual Backwash on/off', style: TextStyle(fontSize: 10),),
-                        style: FilledButton.styleFrom(
-                          backgroundColor: Theme.of(context).primaryColorLight,
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
-                          textStyle: const TextStyle(fontSize: 16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                      )
-                  )
-              ),
-            ]
-        );
-      }),
-    );
-  }
-}
