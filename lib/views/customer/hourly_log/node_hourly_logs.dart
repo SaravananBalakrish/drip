3eb3a6f916c2fc5233f8621a476fdbc2d088431b
Kamarajniagara <niagara.kamaraj@gmail.com>
2026-03-12 10:36:39 +0530

    code updated

--- lib/views/customer/hourly_log/node_hourly_logs.dart
+++ lib/views/customer/hourly_log/node_hourly_logs.dart
@@ -6,80 +6,120 @@ import '../../../repository/repository.dart';
 import '../../../services/http_service.dart';
 import '../../../view_models/customer/node_hourly_logs_vm.dart';
 
+
 class NodeHourlyLogs extends StatelessWidget {
-  const NodeHourlyLogs( {super.key, required this.userId, required this.controllerId, required this.nodes});
+  const NodeHourlyLogs({
+    super.key,
+    required this.userId,
+    required this.controllerId,
+    required this.nodes,
+  });
+
   final int userId, controllerId;
   final List<NodeListModel> nodes;
 
   @override
   Widget build(BuildContext context) {
-    DateTime selectedDate = DateTime.now();
     return ChangeNotifierProvider(
-      create: (_) => NodeHourlyLogsVm(Repository(HttpService()), userId, controllerId, nodes)..getNodeLogs(selectedDate),
-      child: Consumer<NodeHourlyLogsVm>(
-        builder: (context, viewModel, _) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Hourly power logs'),
-              actions: [
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Text(
-                      "${selectedDate.toLocal()}".split(' ')[0],
-                      style: const TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: IconButton(
-                    icon: const Icon(Icons.calendar_today),
-                    onPressed: () => viewModel.selectDate(context),
-                  ),
-                ),
-              ],
-            ),
-            body: viewModel.nodeDataMap.isNotEmpty?SingleChildScrollView(
-              child: Column(
-                children: viewModel.nodeDataMap.entries.map((entry) {
-                  String nodeId = entry.key;
-                  List<ChartDataLog> chartData = entry.value;
-                  return SfCartesianChart(
-                    primaryXAxis: const CategoryAxis(),
-                    title: ChartTitle(text: 'Device Name: ${chartData.elementAt(0).deviceName}  -  ID: $nodeId'),
-                    legend: const Legend(isVisible: true),
-                    //tooltipBehavior: TooltipBehavior(enable: true),
-                    primaryYAxis: const NumericAxis(
-                      title: AxisTitle(text: 'Percentage'),
-                    ),
-                    series: <LineSeries<ChartDataLog, String>>[
-                      LineSeries<ChartDataLog, String>(
-                        dataSource: chartData,
-                        xValueMapper: (ChartDataLog data, _) => data.hour,
-                        yValueMapper: (ChartDataLog data, _) => data.batteryVoltage,
-                        name: 'Battery Voltage',
-                        dataLabelSettings: const DataLabelSettings(isVisible: true),
-                      ),
-                      LineSeries<ChartDataLog, String>(
-                        dataSource: chartData,
-                        xValueMapper: (ChartDataLog data, _) => data.hour,
-                        yValueMapper: (ChartDataLog data, _) => data.solarVoltage,
-                        name: 'Solar Voltage',
-                        dataLabelSettings: const DataLabelSettings(isVisible: true),
-                      ),
-                    ],
-                  );
-                }).toList(),
-              ),
-            ):
-            const Center(child: Text('Node hourly log not found')),
-          );
-        },
-      ),
+      create: (_) => NodeHourlyLogsVm(Repository(HttpService()), userId, controllerId, nodes,)..getNodeLogs(),
+      child: const _NodeHourlyLogsView(),
     );
   }
 }
+
+
+class _NodeHourlyLogsView extends StatelessWidget {
+  const _NodeHourlyLogsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<NodeHourlyLogsVm>(
+      builder: (context, viewModel, _) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Hourly power logs'),
+            actions: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text(
+                    "${viewModel.selectedDate.toLocal()}".split(' ')[0],
+                    style: const TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.calendar_today),
+                onPressed: () => viewModel.selectDate(context),
+              ),
+            ],
+          ),
+          body: viewModel.nodeDataMap.isEmpty
+              ? const Center(child: Text('Node hourly log not found'))
+              : ListView.builder(
+            itemCount: viewModel.nodeDataMap.length,
+            itemBuilder: (context, index) {
+              final entry =
+              viewModel.nodeDataMap.entries.elementAt(index);
+
+              final String nodeId = entry.key;
+              final List<ChartDataLog> chartData = entry.value;
+
+              if (chartData.isEmpty) {
+                return const SizedBox();
+              }
+
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SizedBox(
+                  height: 350,
+                  child: SfCartesianChart(
+                    key: ValueKey(nodeId),
+                    enableAxisAnimation: false,
+                    primaryXAxis: const CategoryAxis(),
+                    primaryYAxis: const NumericAxis(
+                      title: AxisTitle(text: 'Voltage'),
+                    ),
+                    title: ChartTitle(
+                      text:
+                      'Device: ${chartData.first.deviceName} - ID: $nodeId',
+                    ),
+                    legend: const Legend(isVisible: true),
+                    tooltipBehavior:
+                    TooltipBehavior(enable: true),
+                    series: <LineSeries<ChartDataLog, String>>[
+                      LineSeries<ChartDataLog, String>(
+                        animationDuration: 0,
+                        dataSource: chartData,
+                        xValueMapper: (data, _) => data.hour,
+                        yValueMapper: (data, _) =>
+                        data.batteryVoltage,
+                        name: 'Battery Voltage',
+                        dataLabelSettings:
+                        const DataLabelSettings(isVisible: true),
+                      ),
+                      LineSeries<ChartDataLog, String>(
+                        animationDuration: 0,
+                        dataSource: chartData,
+                        xValueMapper: (data, _) => data.hour,
+                        yValueMapper: (data, _) =>
+                        data.solarVoltage,
+                        name: 'Solar Voltage',
+                        dataLabelSettings:
+                        const DataLabelSettings(isVisible: true),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
 
 class ChartDataLog {
   final String deviceName;
@@ -87,5 +127,10 @@ class ChartDataLog {
   final double batteryVoltage;
   final double solarVoltage;
 
-  ChartDataLog(this.deviceName, this.hour, this.batteryVoltage, this.solarVoltage);
+  ChartDataLog(
+      this.deviceName,
+      this.hour,
+      this.batteryVoltage,
+      this.solarVoltage,
+      );
 }
