47107e8c5f22af690e168143f861f536dc0a5c5b
N.S.Venkatesasn <venkatesan.niagara@gmail.com>
2026-05-13 18:02:03 +0530

    cropweather

---
+++ lib/cropAdvisory/view/crop_weatherScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CropWeatherscreen extends StatefulWidget {
+  const CropWeatherscreen({super.key});
+
+  @override
+  State<CropWeatherscreen> createState() => _CropWeatherscreenState();
+}
+
+class _CropWeatherscreenState extends State<CropWeatherscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
